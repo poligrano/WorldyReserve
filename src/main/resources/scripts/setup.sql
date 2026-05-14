fded6e1e5ff977e743f69c0c113f9ed147a5bfaf
Alessandro Sicignano <alessandro.sicignano@studenti.fauser.edu>
2026-05-14 18:39:58 +0200

    added callable funcs

--- src/main/resources/scripts/setup.sql
+++ src/main/resources/scripts/setup.sql
@@ -21,7 +21,7 @@ CREATE TABLE users (
     pfp BLOB,
     email BIGINT UNSIGNED NOT NULL UNIQUE,
     pass BIGINT UNSIGNED UNIQUE,
-    is_google BOOL NOT NULL,
+    is_google BOOL NOT NULL DEFAULT FALSE,
     CHECK ((is_google AND pass IS NULL) OR (NOT is_google AND pass IS NOT NULL)),
     PRIMARY KEY (id),
     FOREIGN KEY (email) REFERENCES users_email(id)
@@ -55,3 +55,33 @@ CREATE TABLE users_reserve_poi (
         ON DELETE CASCADE
         ON UPDATE RESTRICT
 );
+
+CREATE PROCEDURE insert_user(@name VARCHAR(30), @surname VARCHAR(30), @pfp BLOB, @email VARCHAR(254), @pass BINARY(16))
+BEGIN
+    DECLARE @pass_id BIGINT UNSIGNED;
+    DECLARE @email_id BIGINT UNSIGNED;
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+        BEGIN
+            ROLLBACK;
+        END;
+    START TRANSACTION;
+    INSERT INTO users_pass(pass)
+    VALUES (@pass);
+    SET @pass_id = LAST_INSERT_ID();
+    INSERT INTO users_email(email)
+    VALUES (@email);
+    SET @email_id = LAST_INSERT_ID();
+    INSERT INTO users(name, surname, pfp, email, pass)
+    VALUES (@name, @surname, @pfp, @email_id, @pass_id);
+    COMMIT;
+END;
+
+CREATE FUNCTION get_user_info(@email VARCHAR(254), OUT @pass BINARY(16)) RETURNS BIGINT UNSIGNED
+BEGIN
+    DECLARE uid BIGINT UNSIGNED;
+    SELECT  uid = u.id, @pass = up.pass
+    FROM    users AS u
+            INNER JOIN users_pass AS up ON u.pass = up.id
+            INNER JOIN users_email AS ue ON u.email = ue.id
+    WHERE   ue.email = @email;
+END;
